825bebf393c579adec681b766ab7c10f7bd2e2d1
nicky-mc <cinimod@gmail.com>
2024-10-14 12:51:10 +0100

    first commit

---
+++ page_routing_and_navigation/src/app/About/page.js
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold mb-4">Welcome to my About Page</h1>
+      <p className="text-lg">I am a coding nerdy girl in Norfolk</p>
+    </div>
+  );
+}
