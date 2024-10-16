2fcba8083af80438977221e60a21f5792332f2b3
nicky-mc <cinimod@gmail.com>
2024-10-16 16:40:02 +0100

    .env.local created in prep for final project

--- The-Sparkle-Zone/src/utils/supabaseclient.js
+++ The-Sparkle-Zone/src/utils/supabaseclient.js
@@ -1,5 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
-import connect from "./dbconnection";
+export const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+export const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PASSWORD;
+
 export default function supabase() {
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
   const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PASSWORD;
