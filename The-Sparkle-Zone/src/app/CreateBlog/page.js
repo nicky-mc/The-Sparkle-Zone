62abd8cd8f0fbe828652959dba6bfd5851014196
nicky-mc <cinimod@gmail.com>
2024-10-16 16:48:05 +0100

    .env.local created in prep for final project

--- The-Sparkle-Zone/src/app/CreateBlog/page.js
+++ The-Sparkle-Zone/src/app/CreateBlog/page.js
@@ -3,7 +3,7 @@
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { db } from "@/utils/dbconnection";
-import "../blog.css";
+import "./createBlog.css";
 
 const CreateBlog = () => {
   const [title, setTitle] = useState("");
