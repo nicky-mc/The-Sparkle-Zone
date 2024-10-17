9e17f60a41437d2d73c795d2821319a7100b531a
nicky-mc <cinimod@gmail.com>
2024-10-17 14:28:44 +0100

    tweaks to dbconnection and Blog

--- The-Sparkle-Zone/src/utils/dbconnection.js
+++ The-Sparkle-Zone/src/utils/dbconnection.js
@@ -4,6 +4,18 @@ import pg from "pg";
 // Supabase client setup
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PASSWORD;
+
+console.log("Supabase URL:", supabaseUrl);
+console.log("Supabase Key:", supabaseKey);
+
+if (!supabaseUrl) {
+  throw new Error("supabaseUrl is required.");
+}
+
+if (!supabaseKey) {
+  throw new Error("supabaseKey is required.");
+}
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
 // PostgreSQL connection setup
